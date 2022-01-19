e7cc6d2f4449ca52481e4b13ef753f39c82da77e
Ajay Kushwaha <ajay.kushwah@ditstek.com>
2022-01-19 18:44:02 +0530

    Module,screen,action and json file seeder

--- database/seeders/ScreenSeeder.php
+++ database/seeders/ScreenSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Screen\Screen;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Seeder;
 
 class ScreenSeeder extends Seeder
@@ -15,95 +16,18 @@ class ScreenSeeder extends Seeder
     public function run()
     {
 
-        Screen::create([
-            'moduleId' => 1,
-            'name' => 'Roles & Permissions List',
-        ]);
+       Screen::truncate();
 
-        Screen::create([
-            'moduleId' => 2,
-            'name' => 'Global Codes List',
-        ]);
+       $json = File::get("database/data/screen.json");
+       $screens = json_decode($json);
 
-        Screen::create([
-            'moduleId' => 3,
-            'name' => 'CPT Codes List',
-        ]);
+       foreach ($screens->screens as $key => $value) {
 
+                Screen::create([
+                    'moduleId' => $value->moduleId,
+                    'name' => $value->name,
+                ]);
+       }
 
-        Screen::create([
-            'moduleId' => 5,
-            'name' => 'Programs List',
-        ]);
-
-        Screen::create([
-            'moduleId' => 6,
-            'name' => 'Providers List',
-        ]);
-
-        Screen::create([
-            'moduleId' => 6,
-            'name' => 'Provider Summary',
-        ]);
-
-        Screen::create([
-            'moduleId' => 7,
-            'name' => 'Download Report'
-        ]);
-
-        Screen::create([
-            'moduleId' => 8,
-            'name' => 'Care Coordinators List',
-        ]);
-
-        Screen::create([
-            'moduleId' => 8,
-            'name' => 'Care Coordinator Summary',
-        ]);
-
-        Screen::create([
-            'moduleId' => 9,
-            'name' => 'Patients List',
-        ]);
-
-        Screen::create([
-            'moduleId' => 9,
-            'name' => 'Patient Summary',
-        ]);
-
-        Screen::create([
-            'moduleId' => 11,
-            'name' => 'Communications Dashboard View',
-        ]);
-
-        Screen::create([
-            'moduleId' => 11,
-            'name' => 'Communications List View',
-        ]);
-
-        Screen::create([
-            'moduleId' => 12,
-            'name' => 'Appointment Calendar',
-        ]);
-
-        Screen::create([
-            'moduleId' => 13,
-            'name' => 'Tasks Dashboard View',
-        ]);
-
-        Screen::create([
-            'moduleId' => 13,
-            'name' => 'Tasks List View',
-        ]);
-
-        Screen::create([
-            'moduleId' => 14,
-            'name' => 'General parameters',
-        ]);
-
-        Screen::create([
-            'moduleId' => 15,
-            'name' => 'Audit Time Log',
-        ]);
     }
 }
