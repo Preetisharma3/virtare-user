5b056e5efd8128e167d8027647ed529be0154b7a
abhishekpathania <abhishekpathaniawins@gmail.com>
2022-02-04 10:27:45 +0530

    specialization/network staff

--- app/Http/Controllers/Api/v1/StaffController.php
+++ app/Http/Controllers/Api/v1/StaffController.php
@@ -90,61 +90,14 @@ class StaffController extends Controller
         return (new StaffService)->deleteStaffRole($request, $id); 
     }
 
-    
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function specializationCount()
     {
-        //
+        return (new StaffService)->specializationCount(); 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function networkCount()
     {
-        //
+        return (new StaffService)->networkCount(); 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
