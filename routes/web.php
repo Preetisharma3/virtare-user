3d04138e0806bafb8e940dd9055bd68d009e1d79
priyankajesyal <priyankajesyalwins@gmail.com>
2022-02-07 15:08:44 +0530

    update family

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ $router->group(['middleware' => 'auth:api'], function () use ($router) {
     $router->get('appointment', 'Api\v1\AppointmentController@appointmentList');
 
     $router->post('family', 'Api\v1\PatientController@createFamily');
-    $router->put('family', 'Api\v1\PatientController@createFamily');
+    $router->put('family/{id}', 'Api\v1\PatientController@createFamily');
     $router->get('patientInventory', 'Api\v1\PatientController@listingPatientInventory');
     $router->put('inventory/{id}/link', 'Api\v1\PatientController@inventory');
 
