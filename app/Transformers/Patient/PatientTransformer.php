234b3f486c144304cd039ecdf80762e4d0d55329
priyankajesyal <priyankajesyalwins@gmail.com>
2022-02-10 11:47:59 +0530

    add profile photo in patient transformer

--- app/Transformers/Patient/PatientTransformer.php
+++ app/Transformers/Patient/PatientTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers\Patient;
 
+use Illuminate\Support\Facades\URL;
 use League\Fractal\TransformerAbstract;
 use App\Transformers\User\UserTransformer;
 use App\Transformers\Patient\PatientFlagTransformer;
@@ -47,6 +48,7 @@ class PatientTransformer extends TransformerAbstract
             'vitalField' => $data->name,
             'flagName' => 'jhj',
             'flagColor' => 'fhghg',
+            'profile_photo'=>(!empty($data->user->profilePhoto))&&(!is_null($data->user->profilePhoto)) ? URL::to('/').'/'.$data->user->profilePhoto : "",
             'patientFamilyMember' => fractal()->item($data->family)->transformWith(new PatientFamilyMemberTransformer())->toArray(),
             'emergencyContact' => fractal()->item($data->emergency)->transformWith(new PatientFamilyMemberTransformer())->toArray(),
             'patientFlags' => $data->flags ? fractal()->collection($data->flags)->transformWith(new PatientFlagTransformer())->toArray() : [],
