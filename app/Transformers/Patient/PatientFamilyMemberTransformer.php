ec95a9a7a6c427d94bef4132f9220494310ceaac
priyankajesyal <priyankajesyalwins@gmail.com>
2022-02-14 12:19:51 +0530

    change in patient transformer

--- app/Transformers/Patient/PatientFamilyMemberTransformer.php
+++ app/Transformers/Patient/PatientFamilyMemberTransformer.php
@@ -17,28 +17,28 @@ class PatientFamilyMemberTransformer extends TransformerAbstract
 		if($data->relation){
 			return [
 				'id' => $data->id,
-				'patientId'=>$data->patientId,
-				'fullName' =>ucfirst($data->fullName) ,
-				'gender' => $data->gender->name,
-				'phoneNumber' => $data->phoneNumber,
-				'contactType' => $data->contactTypeId,
-				'contactTime' => $data->contactTimeId,
-				'relation' => $data->relation->name,
-				'email' => $data->user->email,
-				'isPrimary'=>$data->isPrimary,
+				'patientId'=>(!empty($data->patientId))?$data->patientId:'',
+				'fullName' =>(!empty($data->fullName))?ucfirst($data->fullName):'',
+				'gender' => (!empty($data->gender->name))?$data->gender->name:'',
+				'phoneNumber' => (!empty($data->phoneNumber))?$data->phoneNumber:'',
+				'contactType' => (!empty($data->contactTypeId))?$data->contactTypeId:'',
+				'contactTime' => (!empty($data->contactTimeId))?$data->contactTimeId:'',
+				'relation' => (!empty($data->relation->name))?$data->relation->name:'',
+				'email' => (!empty($data->user->email))?$data->user->email:'',
+				'isPrimary'=>(!empty($data->isPrimary))?$data->isPrimary:'',
 				'profile_photo'=>(!empty($data->user->profilePhoto))&&(!is_null($data->user->profilePhoto)) ? URL::to('/').'/'.$data->user->profilePhoto : "",
 			];
 		}
 		else{
 			return [
 				'id' => $data->id,
-				'patientId'=>$data->patientId,
-				'fullName' => ucfirst($data->fullName),
-				'gender' => $data->gender->name,
-				'phoneNumber' => $data->phoneNumber,
-				'contactType' => $data->contactTypeId,
-				'contactTime' => $data->contactTimeId,
-				'email' => $data->email,
+				'patientId'=>(!empty($data->patientId))?$data->patientId:'',
+				'fullName' => (!empty($data->fullName))?ucfirst($data->fullName):'',
+				'gender' => (!empty($data->gender->name))?$data->gender->name:'',
+				'phoneNumber' => (!empty($data->phoneNumber))?$data->phoneNumber:'',
+				'contactType' => (!empty($data->contactTypeId))?$data->contactTypeId:'',
+				'contactTime' => (!empty($data->contactTimeId))?$data->contactTimeId:'',
+				'email' => (!empty($data->email))?$data->email:'',
 				'profile_photo'=>(!empty($data->user->profilePhoto))&&(!is_null($data->user->profilePhoto)) ? URL::to('/').'/'.$data->user->profilePhoto : "",
 			];
 		}
