bb1e625c264be3a6f48fe414fb7ee2d1adbe2df4
priyankajesyal <priyankajesyalwins@gmail.com>
2022-02-14 15:13:48 +0530

    users columns nullable

---
+++ database/migrations/2022_02_14_093649_change_email_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeEmailToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('password')->nullable()->change();
+            $table->string('roleId')->nullable()->change();
+            $table->string('profilePhoto')->nullable()->change();
+            $table->string('emailVerify')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('password');
+            $table->dropColumn('roleId');
+            $table->dropColumn('profilePhoto');
+            $table->dropColumn('emailVerify');
+        });
+    }
+}
