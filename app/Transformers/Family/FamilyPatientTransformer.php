c8a3392bf427922165e10ff9ef45d55b996f453a
abhishekpathania <abhishekpathaniawins@gmail.com>
2022-02-21 18:25:42 +0530

    vital/messages/authorization

--- app/Transformers/Family/FamilyPatientTransformer.php
+++ app/Transformers/Family/FamilyPatientTransformer.php
@@ -17,7 +17,7 @@ class FamilyPatientTransformer extends TransformerAbstract
     protected $defaultIncludes = [
         //
     ];
-    
+
     /**
      * List of resources possible to include
      *
@@ -26,7 +26,7 @@ class FamilyPatientTransformer extends TransformerAbstract
     protected $availableIncludes = [
         //
     ];
-    
+
     /**
      * A Fractal transformer.
      *
@@ -35,24 +35,26 @@ class FamilyPatientTransformer extends TransformerAbstract
     public function transform($data): array
     {
         return [
-           'id'=>$data->patientId,
-           'name'=> ucfirst($data->patients->firstName).' '.ucfirst($data->patients->lastName),
-           'dob'=>$data->patients->dob,
-           'relation'=>$data->relation->name,
-           'gender'=>$data->patients->gender->name,
-           'language'=>$data->patients->language->name,
-           'phoneNumber'=>$data->patients->phoneNumber,
-           'profilePhoto'=>(!empty($data->patients->user->profilePhoto))&&(!is_null($data->patients->user->profilePhoto)) ? str_replace("public","",URL::to('/')).'/'.$data->patients->user->profilePhoto : "",
-           'medicalRecordNumber'=>$data->patients->medicalRecordNumber,
-           'country'=>$data->patients->country->name,
-           'state'=>$data->patients->state->name,
-           'city'=>$data->patients->city,
-           'zipCode'=>$data->patients->zipCode,
-           'appartment'=>$data->patients->appartment,
-           'address'=>$data->patients->address,
-           'isDeviceAdded'=>$data->patients->isDeviceAdded,
-           'isPrimary'=>$data->isPrimary,
-           'isActive'=>$data->patients->isActive,
-		];
+            'id' => $data->patientId,
+            'name' => ucfirst($data->patients->firstName) . ' ' . ucfirst($data->patients->lastName),
+            'dob' => $data->patients->dob,
+            'relation' => $data->relation->name,
+            'gender' => $data->patients->gender->name,
+            'language' => $data->patients->language->name,
+            'phoneNumber' => $data->patients->phoneNumber,
+            'profilePhoto' => (!empty($data->patients->user->profilePhoto)) && (!is_null($data->patients->user->profilePhoto)) ? str_replace("public", "", URL::to('/')) . '/' . $data->patients->user->profilePhoto : "",
+            'medicalRecordNumber' => $data->patients->medicalRecordNumber,
+            'country' => $data->patients->country->name,
+            'state' => $data->patients->state->name,
+            'city' => $data->patients->city,
+            'zipCode' => $data->patients->zipCode,
+            'appartment' => $data->patients->appartment,
+            'address' => $data->patients->address,
+            'isDeviceAdded' => $data->patients->isDeviceAdded,
+            'isPrimary' => $data->isPrimary,
+            'vital' => (!empty($data->vital)) ? $data->vital : '',
+            'message' => (!empty($data->messages)) ? $data->messages : '',
+            'isActive' => $data->patients->isActive,
+        ];
     }
 }
--- app/Transformers/Patient/PatientFamilyMemberTransformer.php
+++ app/Transformers/Patient/PatientFamilyMemberTransformer.php
@@ -29,6 +29,8 @@ class PatientFamilyMemberTransformer extends TransformerAbstract
 				'relationId' => (!empty($data->relationId))?$data->relationId:'',
 				'email' => (!empty($data->user->email))?$data->user->email:'',
 				'isPrimary'=>(!empty($data->isPrimary))?$data->isPrimary:'',
+				'vital'=>(!empty($data->vital))?$data->vital:'',
+				'message'=>(!empty($data->messages))?$data->messages:'',
 				'profile_photo'=>(!empty($data->user->profilePhoto))&&(!is_null($data->user->profilePhoto)) ? URL::to('/').'/'.$data->user->profilePhoto : "",
 			];
 	}
