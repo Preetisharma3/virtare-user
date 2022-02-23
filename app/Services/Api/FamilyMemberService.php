c82d53c0c1d4b89f904216910ba4a2941fc08325
abhishekpathania <abhishekpathaniawins@gmail.com>
2022-02-23 14:36:53 +0530

    family member issue fixed

--- app/Services/Api/FamilyMemberService.php
+++ app/Services/Api/FamilyMemberService.php
@@ -7,8 +7,16 @@ use App\Transformers\Family\FamilyPatientTransformer;
 
 class FamilyMemberService
 {
-    public function listPatient($request,$id){
-        $data = PatientFamilyMember::whereHas('patients')->where('userId',auth()->user()->id)->get();
-        return fractal()->collection($data)->transformWith(new FamilyPatientTransformer())->toArray();;
+    public function listPatient($request, $id)
+    {
+        if ($id) {
+            $data = PatientFamilyMember::whereHas('patients')->where([['patientId', $id], ['userId', auth()->user()->id]])->first();
+            return fractal()->item($data)->transformWith(new FamilyPatientTransformer())->toArray();
+        } elseif (!$id) {
+            $data = PatientFamilyMember::whereHas('patients')->where('userId', auth()->user()->id)->get();
+            return fractal()->collection($data)->transformWith(new FamilyPatientTransformer())->toArray();
+        } else {
+            return response()->json(['message' => trans('messages.unauthenticated')], 401);
+        }
     }
 }
