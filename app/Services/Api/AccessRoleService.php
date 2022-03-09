2a4f7e48c02e385318dc57c175e2441c477b4c3e
Ajay Kushwaha <ajay.kushwah@ditstek.com>
2022-03-09 17:25:23 +0530

    staff action access issue

--- app/Services/Api/AccessRoleService.php
+++ app/Services/Api/AccessRoleService.php
@@ -77,22 +77,23 @@ class AccessRoleService
                 $actions = [];
                 $widgets = [];
             }
-            $action= [];
-            $widget= [];
-            foreach( $actions as $new){
-                $actionidx = $new->actionId;
-                array_push($action,$actionidx);
-            }
-            foreach( $widgets as $new){
-                $widgetidx = $new->widgetId;
-                array_push($widget,$widgetidx);
-            }
-
+            
             $daat = [
-                'actionId'=>$action,
-                'widgetId'=>$widget,
+                'actionId'=>$actions,
+                'widgetId'=>$widgets,
             ];
             return $daat;
+
+            // $action= [];
+            // $widget= [];
+            // foreach( $actions as $new){
+            //     $actionidx = $new->actionId;
+            //     array_push($action,$actionidx);
+            // }
+            // foreach( $widgets as $new){
+            //     $widgetidx = $new->widgetId;
+            //     array_push($widget,$widgetidx);
+            // }
 
             //  $finalAction['action']=fractal()->collection($data)->transformWith(new AssignedRoleActionTransformer())->serializeWith(new \Spatie\Fractalistic\ArraySerializer())->toArray();
             //  $finalWidget['widget']=fractal()->collection($data)->transformWith(new AssignedRoleWidgetTransformer())->serializeWith(new \Spatie\Fractalistic\ArraySerializer())->toArray();
