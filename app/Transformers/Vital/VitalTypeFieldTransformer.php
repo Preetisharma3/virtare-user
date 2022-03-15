c1c907a75768588b6d5cd2eecc74d26b4a31d448
priyankajesyal <priyankajesyalwins@gmail.com>
2022-03-15 10:17:45 +0530

    vital icon

--- app/Transformers/Vital/VitalTypeFieldTransformer.php
+++ app/Transformers/Vital/VitalTypeFieldTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers\Vital;
 
+use Illuminate\Support\Facades\URL;
 use League\Fractal\TransformerAbstract;
 
 class VitalTypeFieldTransformer extends TransformerAbstract
@@ -15,6 +16,7 @@ class VitalTypeFieldTransformer extends TransformerAbstract
 		return [
 			'id' => $data->vitalFieldId,
 			'field' => $data->VitalField->name,
+			'icon' => (!empty($data->vitalField->icon)) && (!is_null($data->vitalField->icon)) ? str_replace("public", "", URL::to('/')) . '/' . $data->vitalField->icon : "",
 		];
 	}
 }
